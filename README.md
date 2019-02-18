72cd15b46b2e3837617ec822404b877081bc9bcc
chwa520 <689541@qq.com>
2019-02-18 18:23:22 +0800

    初始化

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # cloudtalk
 
-#### 项目介绍22
+#### 项目介绍
 专注于 企业云布署 的即时通讯系统，基于teamtalk开源项目二次开发,采用 t-io 内核做为websocket服务端核心技术模块,群集布置效果优化,业务模块与底层分离，能快速与自已现有系统对接。支持音,视频通话,支持 andoird  ios  mac windows h5/小程序 HttpApi  是全网唯一支持全平台布署的互联网即时通讯系统。
+官方技术交流QQ群:6445609
+
+项目详细编译文档与安装文档正在准备中，预计三月初出来。有兴趣的请进群关注.
+
+cloudtalk android 测试包: https://github.com/cloudtalk-im/cloudtalk/releases/tag/1.0
+
 
 #### 软件架构
 ![架构草图](https://images.gitee.com/uploads/images/2019/0103/094012_5bf53aa1_494527.png "imnew.png")
@@ -28,9 +34,10 @@
 
 ![音视频通话](https://images.gitee.com/uploads/images/2019/0103/093720_f3fd5f0c_494527.png "screenshot_20190103_092923.png")
 
- **特别声明** 
+ **特别声明**
  本项目基于 teamtalk(https://github.com/mogutt 已不再更新维护) 项目二次开发并重新开源,本项目与原teamtalk架构和协议不再兼容。cloudtalk是则重于构建互联网对外即时通讯系统，支持企业云布署,多个app可使用同一服务端并相互不干扰。
 
+官方技术交流QQ群:6445609
 
 感谢以下开源项目提供强有力的支持:
 
---
+++ server/src/make_mariadb.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+CUR_DIR=
+check_user() {
+    if [ $(id -u) != "0" ]; then
+        echo "Error: You must be root to run this script, please use root to install im"
+        exit 1
+    fi
+}
+get_cur_dir() {
+    # Get the fully qualified path to the script
+    case $0 in
+        /*)
+            SCRIPT="$0"
+            ;;
+        *)
+            PWD_DIR=$(pwd);
+            SCRIPT="${PWD_DIR}/$0"
+            ;;
+    esac
+    # Resolve the true real path without any sym links.
+    CHANGED=true
+    while [ "X$CHANGED" != "X" ]
+    do
+        # Change spaces to ":" so the tokens can be parsed.
+        SAFESCRIPT=`echo $SCRIPT | sed -e 's; ;:;g'`
+        # Get the real path to this script, resolving any symbolic links
+        TOKENS=`echo $SAFESCRIPT | sed -e 's;/; ;g'`
+        REALPATH=
+        for C in $TOKENS; do
+            # Change any ":" in the token back to a space.
+            C=`echo $C | sed -e 's;:; ;g'`
+            REALPATH="$REALPATH/$C"
+            # If REALPATH is a sym link, resolve it.  Loop for nested links.
+            while [ -h "$REALPATH" ] ; do
+                LS="`ls -ld "$REALPATH"`"
+                LINK="`expr "$LS" : '.*-> \(.*\)$'`"
+                if expr "$LINK" : '/.*' > /dev/null; then
+                    # LINK is absolute.
+                    REALPATH="$LINK"
+                else
+                    # LINK is relative.
+                    REALPATH="`dirname "$REALPATH"`""/$LINK"
+                fi
+            done
+        done
+
+        if [ "$REALPATH" = "$SCRIPT" ]
+        then
+            CHANGED=""
+        else
+            SCRIPT="$REALPATH"
+        fi
+    done
+    # Change the current directory to the location of the script
+    CUR_DIR=$(dirname "${REALPATH}")
+}
+
+
+build_mariadb_devel(){
+    CENTOS_VERSION=$(less /etc/redhat-release)
+    #echo "$OS_VERSION, $OS_BIT bit..." 
+    if [[ $CENTOS_VERSION =~ "7.0" ]]; then
+        yum -y install mariadb-devel
+    else
+        echo "centos version must >=7.0 !!!"
+    fi
+}
+
+check_user
+get_cur_dir
+build_mariadb_devel
